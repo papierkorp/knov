c73ecd8694704884764bc9122ceb0ebff492b5b1
Markus Schiller <m.schiller@ymail.com>
2025-10-24 15:35:08 +0200

    pushed with daily script

--- test/main.go
+++ test/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,13 +19,61 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", handleBase)
-	r.Post("/", handleThemeChange)
+	r.Get("/base", handleBase)
+	r.Get("/settings", handleSettings)
+	r.Post("/settings", handleThemeChange)
+	r.Get("/*", handleCSS)
+	r.Get("/themes/"+string(themeManager.currentTheme.Name)+"/*", handleCSS)
 
 	http.ListenAndServe(":1325", r)
 }
 
+func handleCSS(w http.ResponseWriter, r *http.Request) {
+	// todo: use themespath config
+	cssFile := strings.TrimPrefix(r.URL.Path, "/")
+	cssPath := filepath.Join("themes", string(themeManager.currentTheme.Name), cssFile)
+
+	// todo: test for css files instead of * wildcard
+	if _, err := os.Stat(cssPath); os.IsNotExist(err) {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Printf("cssFile: %s", cssFile)
+	fmt.Printf("cssPath: %s", cssPath)
+	w.Header().Set("Content-Type", "text/css")
+	// w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	// w.Header().Set("Pragma", "no-cache")
+	// w.Header().Set("Expires", "0")
+	http.ServeFile(w, r, cssPath)
+
+}
+
 func handleBase(w http.ResponseWriter, r *http.Request) {
+	err := themeManager.Render(w, "base")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error rendering template: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleSettings(w http.ResponseWriter, r *http.Request) {
+	err := themeManager.Render(w, "settings")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error rendering template: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
 
 func handleThemeChange(w http.ResponseWriter, r *http.Request) {
+	themeName := r.FormValue("theme")
+
+	for _, theme := range themeManager.themes {
+		if string(theme.Name) == themeName {
+			themeManager.setCurrentTheme(theme)
+			break
+		}
+	}
+
+	http.Redirect(w, r, "/settings", http.StatusSeeOther)
 }
--- test/thememanager.go
+++ test/thememanager.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
 // -----------------------------------------------
 // ------------- Define Thememanager -------------
 // -----------------------------------------------
@@ -12,8 +21,9 @@ type ThemeManager struct {
 }
 
 type Theme struct {
-	Name     ThemeName
-	Metadata ThemeMetadata
+	Name      ThemeName
+	Metadata  ThemeMetadata
+	Templates ThemeTemplates
 }
 
 type ThemeName string
@@ -25,26 +35,119 @@ type ThemeMetadata struct {
 	Description string `json:"description"`
 }
 
+type ThemeTemplates struct {
+	base     *template.Template
+	settings *template.Template
+}
+
 func InitThemeManager() {
-	builtinThemeMetadata := ThemeMetadata{
-		Name:        "builtin",
-		Version:     "1.0.0",
-		Author:      "knov team",
-		Description: "default theme",
-	}
+	themeManager = &ThemeManager{}
+	loadAllThemes()
 
-	builtinTheme := Theme{
-		Name:     "builtin",
-		Metadata: builtinThemeMetadata,
-	}
-
-	themeManager.addTheme(builtinTheme)
-	themeManager.setCurrentTheme(builtinTheme)
+	fmt.Printf("thememanager current: %v\n", themeManager.currentTheme.Name)
+	fmt.Printf("thememanager themes: %v\n", themeManager.themes)
 
 }
 
-func loadAllThemes() {
+func loadAllThemes() error {
+	// todo: use themespath config
+	themesDir := "themes"
 
+	entries, err := os.ReadDir(themesDir)
+	if err != nil {
+		return fmt.Errorf("failed to read themes directory: %w", err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			themeName := entry.Name()
+			themeDir := filepath.Join(themesDir, themeName)
+			themeJsonPath := filepath.Join(themeDir, "theme.json")
+
+			_, err := os.Stat(themeJsonPath)
+			if err == nil {
+				// ---------------------- Load Metadata ----------------------
+				data, err := os.ReadFile(themeJsonPath)
+				if err != nil {
+					fmt.Printf("arning could not read theme.json for theme '%s': %v\n", themeName, err)
+					continue
+				}
+
+				var metadata ThemeMetadata
+				err = json.Unmarshal(data, &metadata)
+				if err != nil {
+					fmt.Printf("warning invalid json in theme.json for theme: %s: %v", themeName, err)
+				}
+
+				// ---------------------- Load Templates ----------------------
+				// Load base.gotmpl
+				baseTemplatePath := filepath.Join(themeDir, "base.gotmpl")
+				baseTemplate, err := template.ParseFiles(baseTemplatePath)
+				if err != nil {
+					fmt.Printf("warning could not load base.gotmpl for theme '%s': %v\n", themeName, err)
+					continue
+				}
+
+				// Load settings.gotmpl
+				settingsTemplatePath := filepath.Join(themeDir, "settings.gotmpl")
+				settingsTemplate, err := template.ParseFiles(settingsTemplatePath)
+				if err != nil {
+					fmt.Printf("warning could not load settings.gotmpl for theme '%s': %v\n", themeName, err)
+					continue
+				}
+
+				// ---------------------- Generate Theme ----------------------
+
+				templates := ThemeTemplates{
+					base:     baseTemplate,
+					settings: settingsTemplate,
+				}
+
+				theme := Theme{
+					Name:      ThemeName(themeName),
+					Metadata:  metadata,
+					Templates: templates,
+				}
+
+				err = themeManager.addTheme(theme)
+				if err != nil {
+					fmt.Printf("warning could not add theme: %s: %v", themeName, err)
+					continue
+				}
+
+				// todo: check if another theme is set in config
+				if themeName == "builtin" {
+					themeManager.setCurrentTheme(theme)
+				}
+
+				fmt.Printf("added theme: %s\n", metadata.Name)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (tm *ThemeManager) Render(w http.ResponseWriter, templateName string) error {
+	var template *template.Template
+
+	data := map[string]any{
+		"Title":        templateName,
+		"Themes":       tm.themes,
+		"CurrentTheme": tm.currentTheme.Name,
+	}
+
+	switch templateName {
+	case "base":
+		template = tm.currentTheme.Templates.base
+	case "settings":
+		template = tm.currentTheme.Templates.settings
+	default:
+		return fmt.Errorf("unknown template type: %s", templateName)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return template.Execute(w, data)
 }
 
 // -----------------------------------------------
@@ -52,14 +155,14 @@ func loadAllThemes() {
 // -----------------------------------------------
 
 func (tm *ThemeManager) addTheme(theme Theme) error {
-	// todo validate theme before adding it
+	// todo: validate theme before adding it
 	tm.themes = append(tm.themes, theme)
 
 	return nil
 }
 
 func (tm *ThemeManager) setCurrentTheme(theme Theme) error {
-	// todo validate theme before adding it
+	// todo: validate theme before adding it
 	tm.currentTheme = theme
 
 	return nil
